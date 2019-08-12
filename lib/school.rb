316498aaa2f3c5b539f57503c50690af54698ef8
Jane Wysoki <jane.wysoki@gmail.com>
2019-08-12 17:17:30 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
-# code here!
+class School
+  attr_reader :name, :roster  #attr reader because we never see "school.roster =" in the spec file & you don't want anyone overwriting what you put in the roster; don't want to expose functionality to other people
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(student_name, grade)
+    if @roster.has_key?(grade)
+      @roster[grade] << student_name
+    else
+      @roster[grade] = [student_name]
+    end
+  end
+
+  def grade(grade)
+  end
+
+end
+
+school = School.new("Bayside High School")
